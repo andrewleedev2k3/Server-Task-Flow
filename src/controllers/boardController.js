2283659942fad78c57e500aca377f06cfa36876b
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-21 16:42:18 +0700

    Add model Boards Columns Cards

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,10 +1,10 @@
+import { boardService } from '@/services/boardService'
 import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    res.status(StatusCodes.CREATED).json({
-      message: 'POST from Controller API create new board'
-    })
+    const createdBoard = await boardService.createNew(req.body)
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
     next(error)
   }
