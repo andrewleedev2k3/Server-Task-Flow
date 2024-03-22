f723315c8fe87e36eceaef7ec57ad518fe2055e1
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-22 13:21:57 +0700

    Aggregate Board Detail

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,5 +1,8 @@
 const Joi = require('joi')
 import { GET_DB } from '@/config/mongodb'
+import { cardModel } from '@/models/cardModel'
+import { columnModel } from '@/models/columnModel'
+import { BOARD_TYPE } from '@/utils/constants'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '@/utils/validators'
 import { ObjectId } from 'mongodb'
 
@@ -9,6 +12,10 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
+  type: Joi.string()
+    .valid(BOARD_TYPE.PUBLIC, BOARD_TYPE.PRIVATE)
+    .required()
+    .default(BOARD_TYPE.PUBLIC),
   columnOrderIds: Joi.array()
     .items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE))
     .default([]),
@@ -48,8 +55,32 @@ const getDetail = async (id) => {
   try {
     const result = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .findOne({ _id: new ObjectId(id) })
-    return result
+      .aggregate([
+        {
+          $match: {
+            _id: new ObjectId(id),
+            _destroy: false
+          }
+        },
+        {
+          $lookup: {
+            from: columnModel.COLUMN_COLLECTION_NAME,
+            localField: '_id',
+            foreignField: 'boardId',
+            as: 'columns'
+          }
+        },
+        {
+          $lookup: {
+            from: cardModel.CARD_COLLECTION_NAME,
+            localField: '_id',
+            foreignField: 'boardId',
+            as: 'cards'
+          }
+        }
+      ])
+      .toArray()
+    return result[0] || {}
   } catch (error) {
     throw new Error(error)
   }
