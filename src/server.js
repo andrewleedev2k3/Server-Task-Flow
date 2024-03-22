192132cceb583ce37d66deb06bac162ce0f96123
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-22 12:28:26 +0700

    Add cors

--- src/server.js
+++ src/server.js
@@ -5,8 +5,12 @@ import { CLOSE_DB, CONNECT_DB } from '@/config/mongodb'
 import env from '@/config/environment'
 import { APIs_V1 } from '@/routes/v1'
 import { errorHandlingMiddleware } from '@/middlewares/errorHandlingMiddleware'
+import cors from 'cors'
+import { corsOptions } from '@/config/cors'
 const START_SERVER = () => {
   const app = express()
+
+  app.use(cors(corsOptions))
 
   app.use(express.json())
 
