aa16b68a83f6031992bb0b3d78602d4e4a7507d6
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-23 18:16:42 +0700

    Add domain client again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const WHITELIST_DOMAINS = ['https://task-flow-liard.vercel.app/']
+export const WHITELIST_DOMAINS = ['https://task-flow-liard.vercel.app']
 
 export const BOARD_TYPE = {
   PUBLIC: 'public',
